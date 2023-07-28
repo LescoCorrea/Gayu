6a81a6feeb0aadabc7bdaf3cd35b78f67acd28c4
pascal <jeanpascalcorrea@gmail.com>
2023-07-28 05:03:33 +0000

    first commit

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import { Link } from "react-router-dom";
+import Contacts from '../../components/Contacts/Contacts';
+import Footer from '../../components/Footer/Footer';
+import './Contact.css'
+
+export default function Contact() {
+    return (
+        <div>
+            <Navbar />
+            <div className="container mt-5">
+                <ul class="breadcrumb">
+                    <li><Link to="/" className='breadcumbs'>Accueil /</Link></li>
+                    <li><Link to="/artisan" className='breadcumbs'>Nos Artisans /</Link></li>
+                    <li><Link to="/propos" className='breadcumbs'>A propos de nous /</Link></li>
+                    <li className='breadcumbs'>Contact</li>
+                </ul>
+
+                <Contacts />
+            </div>
+            <Footer />
+        </div>
+    )
+}
